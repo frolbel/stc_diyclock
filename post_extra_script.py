8a7a08f9f33fd9c2b56269e5a4a5364098654284
Alexander Belkin <AlexBelkin@list.ru>
2021-10-24 12:49:47 +0300

    Sync CFLAGS & LINKFLAGS from Makefile to extra_script

--- post_extra_script.py
+++ post_extra_script.py
@@ -10,7 +10,15 @@ env = DefaultEnvironment()
 
 # compiler and linker flags dont work very well in build_flags of platformio.ini - need to set them here
 env.Append(
+    CFLAGS = [
+    "--disable-warning", 126,
+    "--disable-warning", 59,
+    "-DWITH_ALT_LED9",
+    "-DWITHOUT_LEDTABLE_RELOC",
+    "-DSHOW_TEMP_DATE_WEEKDAY"
+    ],
     LINKFLAGS = [
+        "--xram-size", 0
         "--data-loc", 0x30
     ],
     STCGALCMD="/stcgal.py"
